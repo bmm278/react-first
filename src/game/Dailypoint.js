711303d3144be9bc16f241f5b455476ae8931ee0
pohan <clin897@gmail.com>
2022-07-25 16:37:57 +0800

    0725

--- src/game/Dailypoint.js
+++ src/game/Dailypoint.js
@@ -1,52 +1,85 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './Dailypoint.css';
 
+
 function Dailypoint () {
+
+    const [eggpoint, seteggpoint] = useState (0)
+    const [dataCheck, setDataCheck] = useState (true)
+    // const [eggbroken, seteggbroken] = useState({src:"/dailypoint-img/812921.png",dataCheck:true})
+
+    const [eggStates, setEggStates] = useState([0,0,0,0,0]);
+
+    const brokenegg = "/dailypoint-img/44434.png" 
+    const defaultegg = "/dailypoint-img/812921.png" 
+    console.log(brokenegg)
+
+    // const setPoint=()=>{
+    //     return 
+    //     seteggpoint(eggpoint + 10)
+    //     // seteggbroken(/dailypoint-img/44434.png)
+        
+    // }
+
+    const eggClick = (i) =>{
+        // console.log({i})
+        if(dataCheck){
+        const newEggStates = [...eggStates];
+        newEggStates[i] = 1;
+        setEggStates(newEggStates);
+        setDataCheck(false)
+    } else {
+            alert("今天領過囉")
+        }
+        /*
+        console.log(e.target);
+        if(eggbroken.dataCheck){
+            seteggbroken({...eggbroken,src:brokenegg,dataCheck:false})
+        } else {
+            alert("今天領過囉")
+        }
+        */
+    }
+
     return (
         <>
             <div className="mb-4 text-white bgg">
-                <div className="container pt-5">
-                    <div className="text-center pt-5 ">
+                <div className="container pt-5" onClick={()=>{seteggpoint(eggpoint + 10)}} >
+                    <div className="text-center pt-5">
                         <img
                             src="/dailypoint-img/1041602.png"
                             alt=""
+                            data-check={dataCheck}
                             className="eggyellow mb-4 mt-4"
+                            
                         />
                     </div>
                     <div className="text-center">
-                        <img
-                            src="/dailypoint-img/812921.png"
+                        {Array(5).fill(1).map((v,i)=>{
+                            return(
+                                <img
+                                key={i}
+                            src={eggStates[i] ? brokenegg : defaultegg }
                             alt=""
+                            data-check={dataCheck.dataCheck}
                             className="egg"
+                            onClick={(e)=>{eggClick(i)}}
                         />
-                        <img
-                            src="/dailypoint-img/812921.png"
-                            alt=""
-                            className="egg"
-                        />
-                        <img
-                            src="/dailypoint-img/812921.png"
-                            alt=""
-                            className="egg"
-                        />
-                        <img
-                            src="/dailypoint-img/812921.png"
-                            alt=""
-                            className="egg"
-                        />
-                        <img
-                            src="/dailypoint-img/812921.png"
-                            alt=""
-                            className="egg"
-                        />
+                            )
+                        })}
+                        
+                        
+                        
                     </div>
                     <br />
                     <div className="">
                         <h1 className="display-6 text-center">點擊領取</h1>
-                        <h1 className="display-7 text-center ">目前點數: 10</h1>
+                    </div>
+                    <div className="">
+                        <h1 className="display-7 text-center ">目前點數:{eggpoint}</h1>
                     </div>
                     <br />
-                    <div>
+                    <div className="">
                         <p className="lead mb-0 text-center bg-danger">
                             今日已完成兌換囉...
                         </p>
